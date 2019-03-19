4328426b6fcec6aa0ffa7c46fb1b20d2f154e926
Daniel Ko <dankomong@gmail.com>
2019-03-19 12:38:19 -0400

    Finish

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,19 @@
 class BoatingTest
 
+  attr_accessor :name, :status, :student, :instructor
+
+  @@boating_tests = []
+
+  def initialize(name, status, student, instructor)
+    @name = name
+    @status = status
+    @student = student
+    @instructor = instructor
+    @@boating_tests << self
+  end
+
+  def self.all
+    @@boating_tests
+  end
+
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,45 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@instructors = []
+
+  def initialize(name)
+    @name = name
+    @@instructors << self
+  end
+
+  def self.all
+    @@instructors
+  end
+
+  def pass_student(student, test_name)
+    boat_test = BoatingTest.all.find do |boating_test|
+      boating_test.instructor == self && boating_test.student == student && boating_test.name == test_name
+    end
+
+    if boat_test == nil
+      boat_test = BoatingTest.new(test_name, "passed", student, self)
+    else
+      boat_test.status = "passed"
+    end
+
+    return boat_test
+  end
+
+  def fail_student(student, test_name)
+    boat_test = BoatingTest.all.find do |boating_test|
+      boating_test.instructor == self && boating_test.student == student && boating_test.name = test_name
+    end
+
+    if boat_test == nil
+      boat_test = BoatingTest.new(test_name, "failed", student, self)
+    else
+      boat_test.status = "failed"
+    end
+
+    return boat_test
+  end
+
+
 end
