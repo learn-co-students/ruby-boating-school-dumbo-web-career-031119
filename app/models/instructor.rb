27fef208dd0a9cb9bd7e1c85f79e4082cf3fab4a
siberry <siberry226@gmail.com>
2019-03-19 10:35:12 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,29 @@
 class Instructor
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    test = BoatingTest.all.find { |test|
+      test.test_name == test_name && test.student == student } ||
+      BoatingTest.new(student, test_name, "passed", self)
+    test.test_status = "passed"
+    test
+  end
+
+  def fail_student(student, test_name)
+    test = pass_student(student, test_name)
+    test.test_status = "failed"
+    test
+  end
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
+  attr_reader :first_name
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def self.find_student(first_name)
+    all.find {|student| student.first_name = first_name}
+  end
+
+  def student_tests
+    BoatingTest.all.select {|test| test.student == self}
+  end
+
+  def grade_percentage
+    grades = student_tests.map {|test| test.test_status}
+    grades.count('passed').to_f / grades.count.to_f
+  end
 
 end
