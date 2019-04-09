6a561e9d2bb9c3065fc0747e016a43272104c77b
Jerry Velazquez <jvr572@gmail.com>
2019-04-09 10:46:13 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,22 @@
 class Instructor
 
+  attr_accessor :instructor_name, :boating_test
+  @@teacher = []
+
+  def initialize(instructor_name)
+    @instructor_name = instructor_name
+    @@teacher << self
+  end
+
+  def self.all
+    @@teacher
+  end
+
+  def pass_student
+
+  end
+
+  def fail_student
+
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 class Student
 
+  attr_accessor :first_name, :boating_test
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test
+    BoatingTest.new(self, boating_test, status, instuctor)
+  end
+
+  def self.find_student
+    binding.pry
+  end
+
+  def grade_percentage
+
+  end
+
 end
